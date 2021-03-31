870dd3580960c62cf8d09fac73928cafbec8d026
Nikhil <nikhil.kapadia.97@gmail.com>
2021-03-30 21:58:17 -0400

    rebuilt project with correct file names

--- bot-webApp/backend/urls.py
+++ bot-webApp/backend/urls.py
@@ -1,7 +1,7 @@
-"""botApp URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
